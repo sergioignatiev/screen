50de16d2fa112a4a39b6a477a4226318fd95f0b8
SergioIgnatev <ignatevsergey17@gmail.com>
2026-07-05 17:24:42 +0300

    delete big file 2.0

--- app/data/movies.ts
+++ app/data/movies.ts
@@ -12,7 +12,7 @@ export const movies: Movie[] = [
   },{
     id:'pascal',
     title:'Pascal',
-    videoSrc:'/video/pascal.mp4',
+    videoSrc:'https://res.cloudinary.com/slcihrr9/video/upload/v1783260663/tomnjerry_zujpzb.mp4',
     posterSrc:'https://static.wixstatic.com/media/eb377a_0f92f802b2f44a568497f80cca17c4b5~mv2.jpeg/v1/fill/w_1196,h_672,fp_0.50_0.50,q_90,enc_avif,quality_auto/eb377a_0f92f802b2f44a568497f80cca17c4b5~mv2.jpeg',
     subtitlesSrc:'/subtitles.ru.vtt',
     country:'russia',
